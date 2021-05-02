9db731fd9b83a228891e94a754429185bb526111
mitubaEX <g1344955@cse.kyoto-su.ac.jp>
2021-05-02 11:53:10 +0900

    add basic keymap and change README

--- .config/nvim/lua/keymap.lua
+++ .config/nvim/lua/keymap.lua
@@ -1,5 +1,8 @@
+-- basic
+vim.api.nvim_set_keymap('n', '<Leader>w', ':w<CR>', { noremap = true, silent = false }) 
+
 -- Telescope
-vim.api.nvim_set_keymap('n', '<Leader>ff', '<cmd>Telescope find_files<CR>', { noremap = true, silent = false }) 
+vim.api.nvim_set_keymap('n', '<Leader>ff', '<cmd>Telescope find_files find_command=rg,--ignore,--hidden,--files prompt_prefix=🔍<CR>', { noremap = true, silent = false }) 
 vim.api.nvim_set_keymap('n', '<Leader>fg', '<cmd>Telescope live_grep<CR>', { noremap = true, silent = false }) 
 vim.api.nvim_set_keymap('n', '<Leader>fb', '<cmd>Telescope buffers<CR>', { noremap = true, silent = false }) 
 vim.api.nvim_set_keymap('n', '<Leader>fh', '<cmd>Telescope help_tags<CR>', { noremap = true, silent = false }) 
--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # nvim_lua_config
 my lua config
+
+## Requirements
+
+- Neovim nightly
+
+## Installation
+
+- Please install [packer.nvim](https://github.com/wbthomason/packer.nvim)
+- Launch Neovim and execute bellow command.
+
+```
+:PackerInstall
+:PackerCompile
+```
+
+Enjoy!
