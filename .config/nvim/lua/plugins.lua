6e3f04c559061c0c4192bed315423fcce67013b4
mitubaEX <g1344955@cse.kyoto-su.ac.jp>
2021-05-06 17:32:53 +0900

    add galaxyline

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -19,6 +19,202 @@ return require('packer').startup(function()
   use { 'hrsh7th/vim-vsnip' }
   use { 'hrsh7th/vim-vsnip-integ' }
 
+  -- status line
+  use {
+    'glepnir/galaxyline.nvim',
+    branch = 'main',
+    requires = { {'kyazdani42/nvim-web-devicons'}, {'ryanoasis/vim-devicons'} },
+    -- your statusline
+    config = function()
+      local gl = require('galaxyline')
+      local colors = require('galaxyline.theme').default
+      local condition = require('galaxyline.condition')
+      local gls = gl.section
+      gl.short_line_list = {'NvimTree','vista','dbui','packer'}
+
+      gls.left = {
+        {
+          ViMode = {
+            provider = function()
+              -- auto change color according the vim mode
+              local mode_color = {n = colors.red, i = colors.green,v=colors.blue,
+                [''] = colors.blue,V=colors.blue,
+                c = colors.magenta,no = colors.red,s = colors.orange,
+                S=colors.orange,[''] = colors.orange,
+                ic = colors.yellow,R = colors.violet,Rv = colors.violet,
+                cv = colors.red,ce=colors.red, r = colors.cyan,
+                rm = colors.cyan, ['r?'] = colors.cyan,
+              ['!']  = colors.red,t = colors.red}
+              vim.api.nvim_command('hi GalaxyViMode guifg='..mode_color[vim.fn.mode()])
+              return '  '
+            end,
+            highlight = {colors.red,colors.bg,'bold'},
+          },
+        },
+        {
+          GitIcon = {
+            provider = function() return '  ' end,
+            condition = condition.check_git_workspace,
+            separator_highlight = {'NONE',colors.bg},
+            highlight = {colors.violet,colors.bg,'bold'},
+          }
+        },
+
+        {
+          GitBranch = {
+            provider = 'GitBranch',
+            separator = ' ',
+            condition = condition.check_git_workspace,
+            highlight = {colors.violet,colors.bg,'bold'},
+          }
+        },
+        {
+          FileIcon = {
+            provider = 'FileIcon',
+            condition = condition.buffer_not_empty,
+            highlight = {require('galaxyline.provider_fileinfo').get_file_icon_color,colors.bg},
+          },
+        },
+        {
+          FileName = {
+            provider = 'FileName',
+            condition = condition.buffer_not_empty,
+            highlight = {colors.magenta,colors.bg,'bold'}
+          }
+        },
+        {
+          DiagnosticError = {
+            provider = 'DiagnosticError',
+            icon = '  ',
+            highlight = {colors.red,colors.bg}
+          }
+        },
+        {
+          DiagnosticWarn = {
+            provider = 'DiagnosticWarn',
+            icon = '  ',
+            highlight = {colors.yellow,colors.bg},
+          }
+        },
+        {
+          DiagnosticHint = {
+            provider = 'DiagnosticHint',
+            icon = '  ',
+            highlight = {colors.cyan,colors.bg},
+          }
+        },
+        {
+          DiagnosticInfo = {
+            provider = 'DiagnosticInfo',
+            icon = '  ',
+            highlight = {colors.blue,colors.bg},
+          }
+        },
+      }
+      gls.mid = {
+        {
+          ShowLspClient = {
+            provider = 'GetLspClient',
+            condition = function ()
+              local tbl = {['dashboard'] = true,['']=true}
+              if tbl[vim.bo.filetype] then
+                return false
+              end
+              return true
+            end,
+            icon = ' LSP:',
+            highlight = {colors.cyan,colors.bg,'bold'}
+          }
+        },
+        {
+          BufferType = {
+            provider = 'FileTypeName',
+            icon = '|ft:',
+            separator_highlight = {'NONE',colors.bg},
+            highlight = {colors.blue,colors.bg,'bold'}
+          }
+        }
+      }
+
+      gls.right = {
+        {
+          FileEncode = {
+            provider = 'FileEncode',
+            condition = condition.hide_in_width,
+            separator = ' ',
+            separator_highlight = {'NONE',colors.bg},
+            highlight = {colors.green,colors.bg,'bold'}
+          }
+        },
+        {
+          FileFormat = {
+            provider = 'FileFormat',
+            condition = condition.hide_in_width,
+            separator = ' ',
+            separator_highlight = {'NONE',colors.bg},
+            highlight = {colors.green,colors.bg,'bold'}
+          }
+        },
+        {
+          DiffAdd = {
+            provider = 'DiffAdd',
+            condition = condition.hide_in_width,
+            separator = ' ',
+            icon = '  ',
+            highlight = {colors.green,colors.bg},
+          }
+        },
+        {
+          DiffModified = {
+            provider = 'DiffModified',
+            condition = condition.hide_in_width,
+            icon = ' 柳',
+            highlight = {colors.orange,colors.bg},
+          }
+        },
+        {
+          DiffRemove = {
+            provider = 'DiffRemove',
+            condition = condition.hide_in_width,
+            icon = '  ',
+            highlight = {colors.red,colors.bg},
+          }
+        },
+        {
+          LineInfo = {
+            provider = 'LineColumn',
+            separator_highlight = {'NONE',colors.bg},
+            separator = ' ',
+            highlight = {colors.fg,colors.bg},
+          },
+        },
+        {
+          PerCent = {
+            provider = 'LinePercent',
+            separator = ' ',
+            separator_highlight = {'NONE',colors.bg},
+            highlight = {colors.fg,colors.bg,'bold'},
+          }
+        },
+        {
+          ScrollBar = {
+            provider = 'ScrollBar',
+            highlight = {colors.fg,colors.bg,'bold'},
+          }
+        },
+      }
+
+      gls.short_line_right[1] = {
+        BufferIcon = {
+          provider= 'BufferIcon',
+          highlight = {colors.fg,colors.bg}
+        }
+      }
+    end,
+    -- some optional icons
+    requires = {'kyazdani42/nvim-web-devicons', opt = true}
+  }
+
   -- Finder
   use {
     'nvim-telescope/telescope.nvim',
