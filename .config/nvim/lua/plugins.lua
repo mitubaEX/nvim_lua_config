605120509eb07b50fb6cd88c509057c60aee2236
mitubaEX <jun-nakamura@c-fo.com>
2021-10-30 19:29:20 +0900

    change keymap

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -526,6 +526,12 @@ return require('packer').startup(function()
     'karb94/neoscroll.nvim',
     config = function ()
       require('neoscroll').setup()
+      local t = {}
+      -- Syntax: t[keys] = {function, {function arguments}}
+      t['<C-u>'] = {'scroll', {'-vim.wo.scroll', 'true', '100'}}
+      t['<C-d>'] = {'scroll', { 'vim.wo.scroll', 'true', '100'}}
+
+      require('neoscroll.config').set_mappings(t)
     end
   }
 
