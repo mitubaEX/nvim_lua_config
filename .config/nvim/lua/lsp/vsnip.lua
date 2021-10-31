e10bae051885a2e0d443c3d078052a372f97b8ec
mitubaEX <jun-nakamura@c-fo.com>
2021-10-31 20:06:23 +0900

    split vsnip config

---
+++ .config/nvim/lua/lsp/vsnip.lua
@@ -0,0 +1,17 @@
+-- vsnip
+-- Expand
+vim.api.nvim_command([[
+imap <expr> <C-f>   vsnip#expandable()  ? '<Plug>(vsnip-expand)'         : '<C-j>'
+smap <expr> <C-f>   vsnip#expandable()  ? '<Plug>(vsnip-expand)'
+]])
+-- Jump forward or backward
+vim.api.nvim_command([[
+imap <expr> <C-b>   vsnip#available(1)  ? '<Plug>(vsnip-expand-or-jump)' : '<C-b>'
+smap <expr> <C-b>   vsnip#available(1)  ? '<Plug>(vsnip-expand-or-jump)' : '<C-b>'
+]])
+-- If you want to use snippet for multiple filetypes, you can `g:vsnip_filetypes` for it.
+vim.api.nvim_command([[
+let g:vsnip_filetypes = {}
+let g:vsnip_filetypes.javascriptreact = ['javascript']
+let g:vsnip_filetypes.typescriptreact = ['typescript']
+]])
