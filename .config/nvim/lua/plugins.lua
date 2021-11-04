3ea4ab10b595c3aeb5e7a6a2f9ae922cc51e866a
mitubaEX <jun-nakamura@c-fo.com>
2021-11-04 21:49:11 +0900

    add copilot

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -41,6 +41,13 @@ return require('packer').startup(function(use)
     end
   }
   -- use { 'simrat39/symbols-outline.nvim' }
+  use {
+    'github/copilot.vim',
+    config = function ()
+      vim.cmd('imap <silent><script><expr> <C-e> copilot#Accept("<CR>")')
+      vim.cmd('let g:copilot_no_tab_map = v:true')
+    end
+  }
 
   -- snippets
   use { 'rafamadriz/friendly-snippets' }
@@ -150,6 +157,7 @@ return require('packer').startup(function(use)
       vim.api.nvim_set_keymap('n', ';', '<cmd>Telescope buffers<CR>', { noremap = true, silent = false })
       vim.api.nvim_set_keymap('n', '<Leader>g', '<cmd>Telescope grep_string<CR>', { noremap = true, silent = false })
       vim.api.nvim_set_keymap('n', '<Leader>y', ":lua require'telescope.builtin'.registers{}<CR>", { noremap = true, silent = true })
+      vim.api.nvim_set_keymap('n', '<C-g>l', ":lua require'telescope.builtin'.git_bcommits{}<CR>", { noremap = true, silent = true })
     end
   }
   -- file tree
@@ -372,12 +380,12 @@ return require('packer').startup(function(use)
       }
     end
   }
-  use {
-    'lewis6991/spellsitter.nvim',
-    config = function()
-      require('spellsitter').setup()
-    end
-  }
+  -- use {
+  --   'lewis6991/spellsitter.nvim',
+  --   config = function()
+  --     require('spellsitter').setup()
+  --   end
+  -- }
   -- git
   use {
     'lewis6991/gitsigns.nvim',
@@ -404,7 +412,7 @@ return require('packer').startup(function(use)
       vim.api.nvim_set_keymap('n', '<C-g>g', ':Gina grep<CR>', { noremap = true, silent = false })
       -- vim.api.nvim_set_keymap('n', '<C-g>o', ':Gina browse :%<CR>', { noremap = true, silent = false })
       vim.api.nvim_set_keymap('n', '<C-g>b', ':Gina blame<CR>', { noremap = true, silent = false })
-      vim.api.nvim_set_keymap('n', '<C-g>l', ':Gina log %<CR>', { noremap = true, silent = false })
+      -- vim.api.nvim_set_keymap('n', '<C-g>l', ':Gina log %<CR>', { noremap = true, silent = false })
     end
   }
   use {
@@ -499,30 +507,30 @@ return require('packer').startup(function(use)
     end
   }
 
-  use {
-    "folke/todo-comments.nvim",
-    requires = "nvim-lua/plenary.nvim",
-    config = function()
-      require("todo-comments").setup {
-        signs = true, -- show icons in the signs column
-        sign_priority = 8, -- sign priority
-        -- keywords recognized as todo comments
-        keywords = {
-          FIX = {
-            icon = " ", -- icon used for the sign, and in search results
-            color = "error", -- can be a hex color, or a named color (see below)
-            alt = { "FIXME", "BUG", "FIXIT", "ISSUE" }, -- a set of other keywords that all map to this FIX keywords
-            -- signs = false, -- configure signs for some keywords individually
-          },
-          TODO = { icon = " ", color = "info" },
-          HACK = { icon = " ", color = "warning" },
-          WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
-          PERF = { icon = " ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
-          NOTE = { icon = " ", color = "hint", alt = { "INFO" } },
-        },
-      }
-    end
-  }
+  -- use {
+  --   "folke/todo-comments.nvim",
+  --   requires = "nvim-lua/plenary.nvim",
+  --   config = function()
+  --     require("todo-comments").setup {
+  --       signs = true, -- show icons in the signs column
+  --       sign_priority = 8, -- sign priority
+  --       -- keywords recognized as todo comments
+  --       keywords = {
+  --         FIX = {
+  --           icon = " ", -- icon used for the sign, and in search results
+  --           color = "error", -- can be a hex color, or a named color (see below)
+  --           alt = { "FIXME", "BUG", "FIXIT", "ISSUE" }, -- a set of other keywords that all map to this FIX keywords
+  --           -- signs = false, -- configure signs for some keywords individually
+  --         },
+  --         TODO = { icon = " ", color = "info" },
+  --         HACK = { icon = " ", color = "warning" },
+  --         WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
+  --         PERF = { icon = " ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
+  --         NOTE = { icon = " ", color = "hint", alt = { "INFO" } },
+  --       },
+  --     }
+  --   end
+  -- }
   -- filetype
   use {
     'nathom/filetype.nvim',
@@ -539,7 +547,7 @@ return require('packer').startup(function(use)
     end
   }
 
-  use { 'dstein64/nvim-scrollview' }
+  -- use { 'dstein64/nvim-scrollview' }
 
   -- my util plugins
   use { 'mitubaEX/blame_open.nvim' }
