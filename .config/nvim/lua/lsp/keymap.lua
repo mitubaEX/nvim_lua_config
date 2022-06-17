ae1f9dceea95415437981f96370948491ab3d790
mitubaEX <jun-nakamura@c-fo.com>
2022-06-18 00:28:37 +0900

    fix config

--- .config/nvim/lua/lsp/keymap.lua
+++ .config/nvim/lua/lsp/keymap.lua
@@ -5,6 +5,7 @@ vim.keymap.set('n', 'gl', ":lua require'telescope.builtin'.lsp_references{}<CR>"
 vim.keymap.set('n', 'K', ':lua vim.lsp.buf.hover()<CR>', { noremap = true, silent = true })
 vim.keymap.set('n', 'gr', ':lua vim.lsp.buf.rename()<CR>', { noremap = true, silent = true })
 vim.keymap.set('n', 'ga', ":lua require'telescope.builtin'.lsp_code_actions{}<CR>", { noremap = true, silent = true })
+vim.keymap.set('n', '<space>f', ":lua vim.lsp.buf.format { async = true }<CR>", { noremap = true, silent = true })
 
 -- vim.keymap.set('n', '<Leader>W', ':lua vim.lsp.buf.formatting_sync(nil, 400)<CR>', { noremap = true, silent = false })
 
--- .config/nvim/lua/lsp/language.lua
+++ .config/nvim/lua/lsp/language.lua
@@ -45,33 +45,20 @@ require "lsp_signature".setup({
   }
 })
 
--- lua
-local system_name
-if vim.fn.has("mac") == 1 then
-  system_name = "macOS"
-elseif vim.fn.has("unix") == 1 then
-  system_name = "Linux"
-elseif vim.fn.has('win32') == 1 then
-  system_name = "Windows"
-else
-  print("Unsupported system for sumneko")
-end
-
--- get home directory
-local handle = io.popen('echo $HOME')
-local home_path = handle:read("*a")
-handle:close()
-
--- set the path to the sumneko installation; if you previously installed via the now deprecated :LspInstall, use
-local sumneko_root_path = home_path:sub(0, -2) .. '/.ghq/src/github.com/sumneko/lua-language-server'
-local sumneko_binary = sumneko_root_path.."/bin/"..system_name.."/lua-language-server"
-
 -- lua-dev.nvim
 local luadev = require("lua-dev").setup({
   lspconfig = {
-    cmd = {sumneko_binary, "-E", sumneko_root_path .. "/main.lua"},
+    -- cmd = {sumneko_binary, "-E", sumneko_root_path .. "/main.lua"},
     capabilities = capabilities
   },
 })
 
 require'lspconfig'.sumneko_lua.setup(luadev)
+
+-- null-ls
+require("null-ls").setup({
+  sources = {
+    require("null-ls").builtins.diagnostics.eslint,
+    require("null-ls").builtins.formatting.eslint,
+  },
+})
