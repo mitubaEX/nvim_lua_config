11de7443def0d0d614cf39ec4a237b8a2871ffd7
mitubaEX <jun-nakamura@c-fo.com>
2022-08-11 16:43:08 +0900

    fix command of copy filename

--- .config/nvim/lua/keymap.lua
+++ .config/nvim/lua/keymap.lua
@@ -28,9 +28,9 @@ vim.keymap.set('n', '<Leader>o', 'o<Esc>', { noremap = true, silent = false })
 
 -- pbcopy filename
 -- only filename
-vim.keymap.set('n', '<Leader>c', ':!echo -n "%:t" | pbcopy<CR>', { noremap = false, silent = false })
+vim.keymap.set('n', '<Leader>c', ':let @*=expand("%:t") | echo expand("%:t")<CR>', { noremap = false, silent = false })
 -- relative path
-vim.keymap.set('n', '<Leader>C', ':!echo -n "%" | pbcopy<CR>', { noremap = false, silent = false })
+vim.keymap.set('n', '<Leader>C', ':let @*=expand("%") | echo expand("%")<CR>', { noremap = false, silent = false })
 
 -- save terminal output to file
 vim.keymap.set('n', '<Leader>>', ':w !tee >> terminal_output.txt<CR>', { noremap = false, silent = false })
