37a912db30d7c194e9bc81807a20017c5b10ce21
mitubaEX <jun-nakamura@c-fo.com>
2022-08-12 09:49:44 +0900

    disable show_current_context_start

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -356,7 +356,7 @@ return require('packer').startup(function(use)
       require("indent_blankline").setup {
         -- for example, context is off by default, use this to turn it on
         show_current_context = true,
-        show_current_context_start = true,
+        show_current_context_start = false,
       }
     end
   }
