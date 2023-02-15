1297a3eb84f3663e59c5d199f3d4b65173c863ee
mitubaEX <jun-nakamura@c-fo.com>
2023-02-15 22:08:38 +0900

    remove sumneko_lua

--- .config/nvim/lua/lsp/language.lua
+++ .config/nvim/lua/lsp/language.lua
@@ -51,26 +51,6 @@ local runtime_path = vim.split(package.path, ';')
 table.insert(runtime_path, 'lua/?.lua')
 table.insert(runtime_path, 'lua/?/init.lua')
 
-lspconfig.sumneko_lua.setup {
-  capabilities = capabilities,
-  settings = {
-    Lua = {
-      runtime = {
-        -- Tell the language server which version of Lua you're using (most likely LuaJIT)
-        version = 'LuaJIT',
-        -- Setup your lua path
-        path = runtime_path,
-      },
-      diagnostics = {
-        globals = { 'vim' },
-      },
-      workspace = { library = vim.api.nvim_get_runtime_file('', true) },
-      -- Do not send telemetry data containing a randomized but unique identifier
-      telemetry = { enable = false, },
-    },
-  },
-}
-
 -- null-ls
 local null_ls = require("null-ls")
 null_ls.setup({
