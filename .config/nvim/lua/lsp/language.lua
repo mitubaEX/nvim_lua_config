f68dd6e6e52ae415790203924056e52d3f52c4a1
mitubaEX <jun-nakamura@c-fo.com>
2023-02-25 21:50:15 +0900

    add ruby-lsp

--- .config/nvim/lua/lsp/language.lua
+++ .config/nvim/lua/lsp/language.lua
@@ -6,47 +6,109 @@ return function()
 
   local lspconfig = require'lspconfig'
 
+  local on_attach = function(client, buffer)
+    local bufopts = { noremap=true, silent=true, buffer=buffer }
+
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+    vim.keymap.set('n', 'gD', ':lua vim.lsp.buf.declaration()<CR>', bufopts)
+    vim.keymap.set('n', 'gl', vim.lsp.buf.references, bufopts)
+    vim.keymap.set('n', 'K', ':lua vim.lsp.buf.hover()<CR>', bufopts)
+    vim.keymap.set('n', 'gr', ':lua vim.lsp.buf.rename()<CR>', bufopts)
+    vim.keymap.set('n', 'ga', vim.lsp.buf.code_action, bufopts)
+    vim.keymap.set('n', '<space>f', ":lua vim.lsp.buf.format { async = true }<CR>", bufopts)
+
+    vim.keymap.set('n', ']d', ":lua vim.diagnostic.goto_next()<CR>", bufopts)
+    vim.keymap.set('n', '[d', ":lua vim.diagnostic.goto_prev()<CR>", bufopts)
+
+    vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(vim.lsp.handlers.hover, {
+      border = 'single',
+    })
+
+    vim.keymap.set('n', '<Leader>xx', '<cmd>TroubleToggle<CR>', bufopts)
+  end
+
   -- lspconfig
   lspconfig.tsserver.setup{
     filetypes = {'typescript', 'typescript.tsx', 'typescriptreact'},
     root_dir = lspconfig.util.root_pattern("package.json", "tsconfig.json", "jsconfig.json"),
     settings = {documentFormatting = false},
+    on_attach = on_attach,
     capabilities = capabilities
   }
   lspconfig.solargraph.setup{
     cmd = { 'bundle', 'exec', 'solargraph', 'stdio' },
     filetypes = {"ruby", "rakefile", "rspec"},
+    on_attach = on_attach,
     capabilities = capabilities
+  }
+  lspconfig.ruby_ls.setup{
+    cmd = { 'bundle', 'exec', 'ruby-lsp' },
+    filetypes = {"ruby", "rakefile", "rspec"},
+    capabilities = capabilities,
+    on_attach = function(client, buffer)
+      local callback = function()
+        local params = vim.lsp.util.make_text_document_params(buffer)
+        client.request(
+        'textDocument/diagnostic',
+        { textDocument = params },
+        function(err, result)
+          if err then return end
+
+          vim.lsp.diagnostic.on_publish_diagnostics(
+          nil,
+          vim.tbl_extend('keep', params, { diagnostics = result.items }),
+          { client_id = client.id }
+          )
+        end
+        )
+      end
+
+      on_attach(client, buffer) -- call my common func
+      callback() -- call on attach
+
+      vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePre', 'BufReadPost', 'InsertLeave', 'TextChanged' }, {
+        buffer = buffer,
+        callback = callback,
+      })
+    end,
   }
   lspconfig.sorbet.setup {
     cmd = { 'bundle', 'exec', 'srb', 'tc', '--lsp' },
     filetypes = {"ruby", "rakefile", "rspec"},
+    on_attach = on_attach,
     capabilities = capabilities,
   }
   lspconfig.flow.setup{
+    on_attach = on_attach,
     capabilities = capabilities
   }
   lspconfig.yamlls.setup{
+    on_attach = on_attach,
     capabilities = capabilities
   }
   lspconfig.rust_analyzer.setup{
+    on_attach = on_attach,
     capabilities = capabilities
   }
   lspconfig.pyright.setup{
+    on_attach = on_attach,
     capabilities = capabilities
   }
   lspconfig.gopls.setup{
+    on_attach = on_attach,
     capabilities = capabilities
   }
+  lspconfig.denols.setup{
+    on_attach = on_attach,
+    root_dir = lspconfig.util.root_pattern("deno.json", "deno.jsonc")
+  }
+
   require "lsp_signature".setup({
     bind = true, -- This is mandatory, otherwise border config won't get registered.
     handler_opts = {
       border = "rounded"
     }
   })
-  lspconfig.denols.setup{
-    root_dir = lspconfig.util.root_pattern("deno.json", "deno.jsonc")
-  }
 
   local runtime_path = vim.split(package.path, ';')
   table.insert(runtime_path, 'lua/?.lua')
