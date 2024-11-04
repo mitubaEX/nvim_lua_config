2e9b5c6235786c146ca954f1661e2e852d385a65
mitubaEX <jun-nakamura@c-fo.com>
2024-11-04 19:11:50 +0900

    fix copilot config

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -32,11 +32,11 @@ return {
     'github/copilot.vim',
     event = "InsertEnter",
     config = function ()
-      vim.keymap.set('i', '<C-J>', 'copilot#Accept("\\<CR>")', {
-        expr = true,
-        replace_keycodes = false
-      })
-      vim.g.copilot_no_tab_map = true
+      -- vim.keymap.set('i', '<C-J>', 'copilot#Accept("\\<CR>")', {
+      --   expr = true,
+      --   replace_keycodes = false
+      -- })
+      -- vim.g.copilot_no_tab_map = true
       vim.g.copilot_filetypes = {
         gitcommit = true,
         markdown = true,
