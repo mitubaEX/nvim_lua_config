d5b065c5008e249036bfab90417aba82ca6bdf86
mitubaEX <jun-nakamura@c-fo.com>
2025-10-28 17:04:36 +0900

    remove luasnip

--- .config/nvim/lua/plugins/completion.lua
+++ .config/nvim/lua/plugins/completion.lua
@@ -6,23 +6,13 @@ return {
 			"hrsh7th/cmp-buffer",
 			"hrsh7th/cmp-path",
 			"hrsh7th/cmp-nvim-lsp",
-			"L3MON4D3/LuaSnip",
-			"saadparwaiz1/cmp_luasnip",
 			"rafamadriz/friendly-snippets",
 			"zbirenbaum/copilot-cmp",
 		},
 		config = function()
 			local cmp = require("cmp")
-			local luasnip = require("luasnip")
-
-			require("luasnip.loaders.from_vscode").lazy_load()
 
 			cmp.setup({
-				snippet = {
-					expand = function(args)
-						luasnip.lsp_expand(args.body)
-					end,
-				},
 				mapping = cmp.mapping.preset.insert({
 					["<C-k>"] = cmp.mapping.select_prev_item(),
 					["<C-j>"] = cmp.mapping.select_next_item(),
@@ -35,7 +25,6 @@ return {
 				sources = cmp.config.sources({
 					{ name = "copilot", priority = 1000 },
 					{ name = "nvim_lsp", priority = 900 },
-					{ name = "luasnip", priority = 800 },
 					{ name = "path", priority = 700 },
 				}, {
 					{ name = "buffer", priority = 600 },
@@ -45,7 +34,6 @@ return {
 						vim_item.menu = ({
 							copilot = "[Copilot]",
 							nvim_lsp = "[LSP]",
-							luasnip = "[Snippet]",
 							buffer = "[Buffer]",
 							path = "[Path]",
 						})[entry.source.name]
